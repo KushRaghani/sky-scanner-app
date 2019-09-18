7793294a3924ad1f385e1361f73b5aa22b3c303d
Kush Raghani <kush.raghani@gmail.com>
2019-09-18 12:25:50 +0530

    adding file-loader to webpack dev config

--- frontend/webpack.config.dev.js
+++ frontend/webpack.config.dev.js
@@ -36,6 +36,14 @@ module.exports = {
       {
         test: /(\.css)$/,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
       }
     ]
   }
